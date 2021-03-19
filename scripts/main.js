63e34115a89dfd48e48b65b8ba8a20af39061b9e
Matheus <matheusvidartesandoval@MacBook-Air-de-Matheus.local>
2021-03-19 08:17:23 -0500

    fixed banner responsive with slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,27 @@
 const onebtn = document.querySelectorAll('.one__btn');
 const rowimg = document.querySelector('.row__img');
+const slider = document.querySelector('.main__slider');
+const menubHamburguer = document.querySelector('.menub__hamburguer')
+const navb = document.querySelector('.menub__links')
+
+let currentSlide = 0;
+
+function handleInterval () {
+    currentSlide++;
+    if(currentSlide >= 2) {
+        currentSlide = 0;
+    }
+    slider.style.transform = `translate(-${100 * currentSlide}%, 0px)`;
+}
+setInterval(handleInterval, 5000);
+
+
+
 
 function handleForEach (elem, i) {
 
     function handleCheck () {
-        console.log(elem,i)
+        
         switch (i) {
             case 0:
                 rowimg.classList.remove('row__img--svg')
